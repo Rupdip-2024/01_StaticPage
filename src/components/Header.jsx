687a294fbe723cbc31b509943f3e52f578a7b092
Cold_20 <some.experiment.2024@gmail.com>
2024-08-04 01:07:12 +0530

    dark mode enabled.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,20 @@
 import Icon from "../images/icon.png"
+import IconDark from "../images/react-dark-icon.png"
 
-export default function Header() {
+export default function Header(props) {
   return(
-      <nav className="nav">
-        <img src={Icon} className='nav--img' />
-        <h3 className="nav--title">ReactFacts</h3>
-        <h4 className="nav--info">React Course-Project 1</h4>
+      <nav className={ props.darkMode ? "nav" : "nav-light"}>
+        <img  src={ props.darkMode ? Icon : IconDark} className={ props.darkMode ? "nav--img" : "light-nav--img" }/>
+        <h3 className={ props.darkMode ? "nav--title" : "nav-light--title"}>ReactFacts</h3>
+        <div className={ props.darkMode ? "nav--info" : "nav-light--info"}>
+          <h5>Dark</h5>
+          <div
+           className={ props.darkMode ? "darkMode-btn" : "lightMode-btn"}
+          >
+            <div className="mode" onClick={props.toggle}></div>
+          </div>
+          <h5>Light</h5>
+        </div>
       </nav>
   )
 }
