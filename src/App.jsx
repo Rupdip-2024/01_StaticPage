9a76f4e5af555a780be46cf271075655e56b214d
Cold_20 <some.experiment.2024@gmail.com>
2024-08-04 01:06:25 +0530

    dark mode enabled.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
-import { useState } from 'react'
+import React from 'react'
 import './App.css'
 import Header from "./components/Header"
 import MainContent from "./components/MainContent"
 
 
-function App() {
+
+export default function App() {
+
+const [darkMode, setDarkMode] = React.useState(true)
+
+function toggle() {
+  setDarkMode(prevDarkMode => !prevDarkMode)
+}
+
   return (
     <>
       <div>
-        <Header />
-        <MainContent />
+        <Header 
+        toggle={toggle}
+        darkMode={darkMode}
+        />
+        <MainContent 
+        toggle={toggle}
+        darkMode={darkMode}
+        />
       </div>
     </>
   )
 }
-
-export default App
